2eecbef1c610c199d33d4be4a9ae388155b42084
Neetesh Keshari <neetesh.keshari@akqa.com>
2018-04-16 08:53:21 +0530

    updated comments

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,9 @@ module.exports = {
 			{
 				test: /\.scss$/,
 				use: [
-					{loader: 'style-loader'},
-					{loader: 'css-loader'},
-					{loader: 'sass-loader'}
+					{loader: 'style-loader'},	// creates style nodes from JS strings
+					{loader: 'css-loader'},		// translates CSS into CommonJS
+					{loader: 'sass-loader'}		// compiles Sass to CSS
 				]
 			},
 			{
